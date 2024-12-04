49247a2abd18f7bedf5e7116b2686b5d50749988
Timur Sibgatullin <trsibgatullin@kpfu.ru>
2024-12-04 13:25:35 +0300

    class User updated - TimurS

--- src/main/java/org/example/model/User.java
+++ src/main/java/org/example/model/User.java
@@ -1,4 +1,55 @@
 package org.example.model;
-
+//- Пользователь
+//    - ник найм
+//    - имя пользователя
+//    - телефон
+//    - пароль
 public class User {
+    private String nickname;
+    private String name;
+    private String mobilePhone;
+    private String password;
+
+    public User() {}
+
+    public User(String nickname, String name, String mobilePhone, String password) {
+        this.nickname = nickname;
+        this.name = name;
+        this.mobilePhone = name;
+        this.password = name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setNickname(String nickname) {
+        this.nickname = nickname;
+    }
+
+    public void setMobilePhone(String mobilePhone) {
+        this.mobilePhone = mobilePhone;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getNickname() {
+        return nickname;
+    }
+
+    public String getMobilePhone() {
+        return mobilePhone;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
 }
+

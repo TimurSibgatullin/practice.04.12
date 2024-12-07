756cc64b379334d213ae1a52a16ea5d21661746b
Kanan <you@example.com>
2024-12-07 11:18:30 +0300

    "kananchik"

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -9,6 +9,9 @@ public class Main {
         for (int i = 0; i < arr.length; i++) {
             System.out.println(arr[i].getName() + arr[i].getNickname() + arr[i].getMobilePhone() + arr[i].getPassword());
         }
-
+        Message[] arr2 = MessageService.create4Message();
+        for (int i = 0; i < arr2.length; i++) {
+            System.out.println(arr2[i].getSender() + arr2[i].getRecipient() + arr2[i].getMessage() + arr2[i].getTime());
+        }
     }
 }
--- src/main/java/org/example/model/MessageService.java
+++ src/main/java/org/example/model/MessageService.java
@@ -7,5 +7,7 @@ public class MessageService {
     public static Message[] create4Message(User[] arr) {
         Message message1 = new Message(arr[0],arr[1],"hi");
         Message message2 = new Message(arr[1],arr[0],"hihi");
+        Message[] arr5 = {message1, message2};
+        return arr5;
     }
 }

5df2015ae33e0a018e2a5aa0d2229776416c1e6b
Timur Sibgatullin <trsibgatullin@kpfu.ru>
2024-12-07 11:52:25 +0300

    class main minor updates

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -9,7 +9,7 @@ public class Main {
     public static void main(String[] args) {
         User[] arr = UserService.create4Users();
         for (int i = 0; i < arr.length; i++) {
-            System.out.println(arr[i].getName() + arr[i].getNickname() + arr[i].getMobilePhone() + arr[i].getPassword());
+            System.out.println(arr[i].getName() + " " + arr[i].getNickname() + " " + arr[i].getMobilePhone() + " " + arr[i].getPassword());
         }
         Message[] arr2 = MessageService.create4Message(arr);
         for (int i = 0; i < arr2.length; i++) {
